eaf08b4b3781bbc8cfa36b81206e3000530092fc
slava <slava@satellogic.com>
2018-08-03 23:20:42 +0300

    pypi-ready setup

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 import os
 from setuptools import setup, find_packages
 
+version = '0.1.0.dev0'
+
 proj_dir = os.path.abspath(os.path.dirname(__file__))
 reqs = [line.strip()
-        for line in open(os.path.join(proj_dir, 'requirements.txt'), encoding='utf-8')]
+        for line in open(
+                os.path.join(proj_dir, 'requirements.txt'), encoding='utf-8')]
 
 setup(
     name='calval',
-    version='0.1.0.dev0',
-    maintainer_email='amit@satellogic.com',
+    version=version,
+    author='Slava Kerner, Amit Aronovitch',
+    url='https://github.com/satellogic/calval',
+    author_email='amit@satellogic.com',
+    description="""\
+Python package for easy radiometric calibration&validation of satellite EO payloads
+""",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
     packages=find_packages(),
     package_data = {
         'calval': ['site_data/*']
