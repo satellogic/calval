9c2c50d9e03f6ace7f2358ef69703edfade12b63
Amit Aronovitch <amit@satellogic.com>
2018-11-04 23:57:11 +0200

    add TilePile (tile comparison & median)

--- calval/scene_utils.py
+++ calval/scene_utils.py
@@ -1,7 +1,10 @@
 import logging
 from collections import OrderedDict
+import numpy as np
 import pandas as pd
-from calval.sites import get_site_aoi
+from calval.utils import cached_property
+from calval.raster_utils import uncached_get_tile
+from calval.sites import get_site_aoi, site_tile
 from calval.sat_measurements import SatMeasurements
 from calval.normalized_scene import band_names
 from calval.providers import SceneInfo, SceneData
@@ -11,6 +14,58 @@ import calval.providers.landsat  # noqa: F401
 
 
 logger = logging.getLogger(__name__)
+
+
+class TilePile:
+    """
+    pixel-by-pixel comparison of several scenes by extraction of common tile
+    Can compute the median tile (a numpy maskedarray, with the median of each pixel)
+    and statistics of relative distances from a reference tile.
+    """
+    def __init__(self, scenes, band, site_name, base_zoomlevel=13, zoomlevel=None, get_tile=uncached_get_tile):
+        """
+        Extract a common tile (bounds determined by coords of the site `site_name`,
+        and `base_zoomlevel`, resolution specified by `zoomlevel`)
+        """
+        self.band = band
+        self.scenes = scenes
+        self.tile_coords = site_tile(site_name, base_zoomlevel)
+        if zoomlevel is None:
+            zoomlevel = base_zoomlevel
+        self.zoomlevel = zoomlevel
+        tiles = [
+            scene.band_tile(band, self.tile_coords, zoomlevel=zoomlevel, get_tile=get_tile)
+            for scene in scenes
+        ]
+        alltiles = np.ma.concatenate([x.image for x in tiles], axis=0)
+        self.raster = tiles[0].copy_with(
+            image=alltiles, band_names=[str(scene.scene_info) for scene in scenes])
+
+    @cached_property
+    def median(self):
+        median = np.ma.median(self.raster.image, axis=0)
+        return self.raster.copy_with(image=median, band_names=[self.band])
+
+    def quantile_abs_reldiff(self, ref_band, q):
+        """
+        quantile `q` of abs(rel distance) from `ref_band` (masked array)
+        """
+        absdiff = np.ma.abs(self.raster.image - ref_band) / ref_band
+        return np.nanquantile(absdiff.filled(np.nan), q)
+
+    def quantile_reldiff(self, ref_band, q):
+        """quantile `q` of relative distance from `ref_band` (masked array)"""
+        diff = (self.raster.image - ref_band) / ref_band
+        return np.nanquantile(diff.filled(np.nan), q)
+
+    # TODO: for small number of rasters (in particular 2 & 3), need self-distance from median.
+    # correct way is to do weighted quantile, giving weight=0.5 for two mid-values if even, and
+    # weight=0 for the mid-value if odd. This requires sorting etc.
+    def self_abs_reldiff_quantile(self, q):
+        return self.quantile_abs_reldiff(self.median.image, q)
+
+    def self_reldiff_quantile(self, q):
+        return self.quantile_reldiff(self.median.image, q)
 
 
 def make_sat_measurements(scenes, site_name, product, label=None, bands=band_names, provider=None,
--- tests/test_scene_utils.py
+++ tests/test_scene_utils.py
@@ -1,12 +1,22 @@
 import os
+import pytest
 import numpy as np
-from testing_utils import config
+from testing_utils import config, normalize_folders_into
 from calval.providers import SceneInfo
-from calval.scene_utils import make_sat_measurements
+from calval.scene_utils import make_sat_measurements, TilePile
 from calval.normalized_scene import band_names
+from calval.storage import FileStorage
 
 expected_blue_toa = {'landsat8': 0.21, 'sentinel2': 0.23}
 expected_blue_sr = {'landsat8': 0.19}
+
+
+@pytest.fixture(scope='module')
+def norm_scene_storage(module_temp_dir):
+    module_temp_dir = '/tmp'
+    normpath = os.path.join(module_temp_dir, 'normalized')
+    normalize_folders_into(normpath)
+    return FileStorage(normpath)
 
 
 def test_make_sat_measurements():
@@ -28,3 +38,18 @@ def test_make_sat_measurements():
                            atol=0.01)
         std_names = ['{}_std'.format(x) for x in band_names]
         assert np.allclose(rows[std_names], 0, atol=0.05)
+
+
+def test_tile_pile(norm_scene_storage):
+    scenes = norm_scene_storage.query(product='toa')
+    pile = TilePile(scenes, 'green', 'negev', 10)
+    assert len(scenes) == pile.raster.image.shape[0]
+    assert len(scenes) == len(pile.raster.band_names)
+    assert pile.median.band_names == ['green']
+    assert np.ma.average(pile.median.image) == pytest.approx(0.2658, rel=1e-3)
+    q = pile.quantile_abs_reldiff(pile.median.image, np.arange(0, 1 + 1e-5, 0.25))
+    assert q[0] == 0
+    q = pile.quantile_reldiff(pile.median.image, np.arange(0, 1 + 1e-5, 0.25))
+    assert q[2] == 0
+    assert pile.self_abs_reldiff_quantile(0) == 0
+    assert pile.self_reldiff_quantile(0.5) == 0
